ddd805e573f6a853a7bec886e1029e6aafea841c
Revotadas <rafael.lopes24@gmail.com>
2022-07-07 10:53:05 +0100

    Modify products

--- SuperShop/Data/Entities/Product.cs
+++ SuperShop/Data/Entities/Product.cs
@@ -5,6 +5,8 @@ namespace SuperShop.Data.Entities {
     public class Product {
         public int Id { get; set; }
 
+        [Required]
+        [MaxLength(50, ErrorMessage = "The firld {0} can contain {1} characters.")]
         public string Name { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
@@ -13,13 +15,13 @@ namespace SuperShop.Data.Entities {
         [Display(Name = "Image")]
         public string ImageUrl { get; set; }
 
-        [Display(Name="Last Purchase")]
-        public DateTime LastPurchase { get; set; }
+        [Display(Name = "Last Purchase")]
+        public DateTime? LastPurchase { get; set; }
 
         [Display(Name = "Last Sale")]
-        public DateTime LastSale { get; set; }
+        public DateTime? LastSale { get; set; }
 
-        [Display(Name="Is Available")]
+        [Display(Name = "Is Available")]
         public bool IsAvailable { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
