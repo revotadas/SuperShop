a0170dba401c230141e93f771438500b6daed106
Revotadas <rafael.lopes24@gmail.com>
2022-07-12 12:40:37 +0100

    Api Products Controller

---
+++ SuperShop/Controllers/API/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperShop.Data;
+
+namespace SuperShop.Controllers.API {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : Controller {
+        private readonly IProductsRepository productsRepository;
+
+        public ProductsController(IProductsRepository productsRepository) {
+            this.productsRepository = productsRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts() {
+            return Ok(productsRepository.GetAll());
+        }
+    }
+}
